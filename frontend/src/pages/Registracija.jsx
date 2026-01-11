5ab0a23726a1b0108b3fe32cb740c276d2f62a93
Nejla Buljina <nbuljina1@etf.unsa.ba>
2026-01-11 13:10:11 +0100

    formeprijavareg

--- frontend/src/pages/Registracija.jsx
+++ frontend/src/pages/Registracija.jsx
@@ -1,8 +1,148 @@
+import { useState } from "react";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+
+
 export default function Registracija() {
+  const [ime, setIme] = useState("");
+  const [sifra, setSifra] = useState("");
+  const [uloga, setUloga] = useState("student");
+  const [showPassword, setShowPassword] = useState(false);
+
+
+  const handleSubmit = (e) => {
+    if (sifra.length !== 4) {
+      alert("Greška: šifra mora imati tačno 4 karaktera!");
+      return;
+    }
+
+    e.preventDefault();
+
+    // Za sada samo provjera u konzoli
+    console.log("Registracija:");
+    console.log("Ime:", ime);
+    console.log("Šifra:", sifra);
+    console.log("Uloga:", uloga);
+
+    alert("Registracija uspješna");
+  };
+
   return (
-    <div style={{ padding: "40px" }}>
-      <h1>Registracija</h1>
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#3a3485",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "white"
+      }}
+    >
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          background: "#2f2a70",
+          padding: "40px",
+          borderRadius: "20px",
+          width: "320px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "15px"
+        }}
+      >
+        <h2 style={{ textAlign: "center" }}>Registracija</h2>
+
+        <input
+          type="text"
+          placeholder="Ime"
+          value={ime}
+          onChange={(e) => setIme(e.target.value)}
+          required
+          style={inputStyle}
+        />
+
+        <div style={{ position: "relative", width: "100%" }}>
+  	  <input
+    	    type={showPassword ? "text" : "password"}
+    	    placeholder="●●●●"
+    	    value={sifra}
+    	    onChange={(e) => setSifra(e.target.value.slice(0, 4))}
+    	    maxLength={4}
+    	    required
+    	    style={{
+      	      width: "100%",
+      	      padding: "10px 40px 10px 12px",
+      	      borderRadius: "8px",
+      	      border: "1px solid #ccc",
+      	      fontSize: "1rem",
+              boxSizing: "border-box" //da input ne prelazi sirinu forme
+    	    }}
+  	  />
+          {sifra.length == 0 && (
+            <span style={{ fontSize: "0.85rem", color: "#ccc" }}>
+	      Unesite 4 karaktera
+            </span>
+          )}
+
+ 	  {/* Show / Hide dugme */}
+  	  <button
+    	    type="button"
+    	    onClick={() => setShowPassword(!showPassword)}
+    	    style={{
+      	      position: "absolute",
+      	      right: "10px",
+      	      top: "50%",
+      	      transform: "translateY(-50%)",
+      	      background: "none",
+      	      border: "none",
+      	      cursor: "pointer",
+      	      fontSize: "1.3rem",
+	      color: "#333"
+    	    }}
+  	  >
+    	    {showPassword ? <AiFillEye /> : <AiFillEyeInvisible />}
+  	  </button>
+	</div>
+
+	{sifra.length > 0 && sifra.length !== 4 && (
+  	  <span style={{ color: "#ffb3b3", fontSize: "0.85rem" }}>
+    	    Unesite tačno 4 karaktera!
+          </span>
+	)}
+
+
+
+        <select
+          value={uloga}
+          onChange={(e) => setUloga(e.target.value)}
+          style={inputStyle}
+        >
+          <option value="student">Student</option>
+          <option value="pacijent">Pacijent</option>
+        </select>
+
+        <button type="submit" style={buttonStyle}>
+          Registruj se
+        </button>
+      </form>
     </div>
   );
 }
+
+const inputStyle = {
+  padding: "10px",
+  borderRadius: "10px",
+  border: "none",
+  fontSize: "1rem"
+};
+
+const buttonStyle = {
+  padding: "12px",
+  borderRadius: "20px",
+  border: "none",
+  fontSize: "1rem",
+  cursor: "pointer",
+  background: "white",
+  color: "#3a3485",
+  fontWeight: "bold"
+};
 
